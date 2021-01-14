6fc6565528e1e3d8e2acd8b7c621379355bae22b
Wiliam Nascimento <wilnasc990s3@gmail.com>
2021-01-14 16:19:27 -0300

    Desafio 2 completo.

--- Solucao_de_problemas_essenciais/desafio2.js
+++ Solucao_de_problemas_essenciais/desafio2.js
@@ -1,18 +1,19 @@
-let numeroTartarugasGrupo = 10;
-let velocidadesTarturugas = '19 9 1 4 5 8 6 11 9 7'.split(' ');
-console.log(velocidadesTarturugas);
-
-let maiorVelocidadeTartarugaGrupo = velocidadesTarturugas.reduce((velocidadeMaxima, velocidade) => {
-    return Math.max(velocidadeMaxima, velocidade);
-}, 0);
-console.log(maiorVelocidadeTartarugaGrupo);
-
-let nivelTartaruga = (velocidade) => {
-    if (velocidade < 10)
+for (let i = 0; i < 3; i++){
+    let numeroTartarugasGrupo = gets();
+    let velocidadeTartarugas = gets().split(' ');
+    
+    let maiorVelocidadeTartarugaGrupo = velocidadeTartarugas.reduce((velocidadeMaxima, velocidade) => {
+      return Math.max(velocidadeMaxima, velocidade);
+    }, 0);
+    
+    let nivelTartaruga = (velocidade) => {
+      if (velocidade < 10)
         return 1;
-    if (velocidade >= 10 && velocidade < 20)
+      else if (velocidade >= 10 && velocidade < 20)
         return 2;
-    else
+      else
         return 3;
-};
-console.log(nivelTartaruga(maiorVelocidadeTartarugaGrupo));
+    };
+    
+    console.log(nivelTartaruga(maiorVelocidadeTartarugaGrupo));
+  }
