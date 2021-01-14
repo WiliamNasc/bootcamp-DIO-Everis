2da584dbdc5aa9baf348d85b6349edf3dfe411df
Wiliam Nascimento <wilnasc990s3@gmail.com>
2021-01-14 16:30:42 -0300

    Adição do Desafio 1.

---
+++ Solucao_de_problemas_essenciais/desafio1.js
@@ -0,0 +1,7 @@
+let numero = 5;
+
+for (let i = 1; i <= numero; i++) {
+    let numeroAoQuadrado = Math.pow(i, 2);
+    let numeroAoCubo = Math.pow(i, 3);
+    console.log(i, numeroAoQuadrado, numeroAoCubo);
+}
